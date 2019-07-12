f40cce112cbd9c127d46cecd7a071a6a67601800
Owen Voke <owzie123@gmail.com>
2019-07-12 16:21:51 +0100

    Add Movie entity class

---
+++ src/Entities/Movie.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace pxgamer\JustWatch\Entities;
+
+use stdClass;
+
+final class Movie extends AbstractEntity
+{
+    /** @var int */
+    public $id;
+    /** @var string */
+    public $title;
+    /** @var string */
+    public $fullPath;
+    /** @var array<string, string> */
+    public $fullPaths;
+    /** @var string */
+    public $poster;
+    /** @var array<int, string> */
+    public $backdrops;
+    /** @var string */
+    public $shortDescription;
+    /** @var int */
+    public $originalReleaseYear;
+    /** @var float */
+    public $tmdbPopularity;
+    /** @var string */
+    public $objectType;
+    /** @var string */
+    public $originalTitle;
+    /** @var array<int, stdClass> */
+    public $offers;
+    /** @var array<int, stdClass> */
+    public $clips;
+    /** @var array<int, stdClass> */
+    public $scoring;
+    /** @var array<int, stdClass> */
+    public $credits;
+    /** @var array<int, stdClass> */
+    public $externalIds;
+    /** @var string */
+    public $originalLanguage;
+    /** @var array<int, int> */
+    public $genreIds;
+    /** @var string */
+    public $ageCertification;
+    /** @var int */
+    public $runtime;
+}
